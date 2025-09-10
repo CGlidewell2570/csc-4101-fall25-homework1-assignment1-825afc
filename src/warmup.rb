ad9c9368f1849bcd13d6f57c504903dd4bf80f46
CGlidewell2570 <cglide2@lsu.edu>
2025-09-09 22:13:31 -0500

    Update warmup.rb
    Edited warmup for Homework 1

--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,49 @@
 def fib(n)
-    raise Exception, "Not Implemented"
+    if n <= 0
+        return []
+    end 
+    if n == 1
+        return [0]
+    end
+    fibArray = [0, 1]
+    (n-2).times do
+        fibArray << fibArray[-1] + fibArray[-2]
+    end
+
+    fibArray
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+    org = n.to_s
+    rev = org.reverse
+    org == rev
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+    return nil if a.empty? || n < 0 || n >= a.size
+    sorted = a.sort.reverse
+    sorted[n]
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+    return "" if s.size == 0
+    counts = Hash.new(0)
+    org = s.split("")
+    org.each {|char| counts[char] += 1}
+
+    counts.max_by {|char, count| count}[0]
 end
 
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+    return nil if arr1.size != arr2.size
+
+    result = {}
+    arr1.each_with_index do |element, index|
+        result[element] = arr2[index]
+    end
+    result
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    hash.to_a
 end
