9f3b98529765b39c93a9bdea302eb50dbe249356
CGlidewell2570 <cglide2@lsu.edu>
2025-09-09 22:12:23 -0500

    Update phonebook.rb
    Updates phonebook for homework 1

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,62 @@
 class PhoneBook
     def initialize
+        @items = []
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        #Check that name doesn't already exist
+        if doesNameExist?(name)
+            return false 
+        end
+
+        #Check that the number is valid format
+        if !isNumberValid?(number)
+            return false
+        end
+
+        #Check that number is not listed true
+        if is_listed && isNumberListed?(number)
+            return false
+        end
+
+        #Add item to the items list
+        @items << {name: name, phone_number: number, listed: is_listed}
+        #Phone number has been successfully added
+        return true
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        person = @items.find {|item| item[:name] == name && item[:listed]}
+        return nil if person.nil?
+        person[:phone_number]
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        person = @items.find {|item| item[:phone_number] == number && item[:listed]}
+        return nil if person.nil?
+        person[:name]
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        return [] if areacode.nil? || areacode.strip.empty?
+        
+        @items.select {|item| item[:phone_number].start_with?("#{areacode}-")}
+        .collect{ |item| item[:name]}
     end
+
+    # Helper methods I created in order to make the code cleaner
+
+    def doesNameExist?(name)
+        @items.any? {|item| item[:name] == name}
+    end
+
+    def isNumberValid?(number)
+        !!(number =~ /^[1-9]\d{2}-\d{3}-\d{4}$/)
+    end
+
+    def isNumberListed?(number)
+        @items.any? {|item| item[:phone_number] == number && entry[:listed]}
+    end
+
+
 end
